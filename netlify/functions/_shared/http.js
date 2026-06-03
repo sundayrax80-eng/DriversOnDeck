29bedcec61e80d757b96620d7c110d61bd200ded
sundayrax80-eng <sundayrax80@gmail.com>
2026-06-02 20:53:57 -0500

    Update http.js

--- netlify/functions/_shared/http.js
+++ netlify/functions/_shared/http.js
@@ -1,29 +1,32 @@
 function json(statusCode, body, headers = {}) {
-  return {
-    statusCode,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": process.env.PUBLIC_SITE_URL || "*",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      ...headers
-    },
-    body: JSON.stringify(body)
-  };
+    return {
+          statusCode,
+          headers: {
+                  "Content-Type": "application/json",
+                  "Access-Control-Allow-Origin": process.env.PUBLIC_SITE_URL || "*",
+                  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+                  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+                  ...headers
+          },
+          body: JSON.stringify(body)
+    };
 }
 
 function parseBody(event) {
-  if (!event.body) return {};
-  try {
-    return JSON.parse(event.body);
-  } catch {
-    return {};
-  }
+    if (!event.body) return {};
+    try {
+          return JSON.parse(event.body);
+    } catch (error) {
+          const err = new Error("Invalid JSON request body");
+          err.statusCode = 400;
+          err.cause = error;
+          throw err;
+    }
 }
 
 function handleOptions(event) {
-  if (event.httpMethod === "OPTIONS") return json(204, {});
-  return null;
+    if (event.httpMethod === "OPTIONS") return json(204, {});
+    return null;
 }
 
 module.exports = { json, parseBody, handleOptions };
